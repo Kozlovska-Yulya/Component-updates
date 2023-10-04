1c4f5fc460054a97b44bd4af24e7d00e2dc8b440
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-10-04 15:49:57 +0300

    Expanded Component

--- task3/src/Expand.jsx
+++ task3/src/Expand.jsx
@@ -1,30 +1,46 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const Expand = ({ children, title }) => {
-  const [isShow, setIsShow] = useState(false);
-
-  const toggleContent = () => {
-    setIsShow(!isShow);
+class Expand extends Component {
+  state = {
+    isExpanded: false,
   };
-  return (
-    <div className="expand border">
-      <div className="expand__header">
-        <span className="expand__title">{title}</span>
-        <button className="expand__toggle-btn" onClick={toggleContent}>
-          <i className="fa-solid fa-chevron-up"></i>
-        </button>
+
+  toggleContent = () => {
+    this.setState({
+      isExpanded: !this.state.isExpanded,
+    });
+  };
+
+  render() {
+    const { isExpanded } = this.state;
+    const { title, children } = this.props;
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{title}</span>
+          <button className="expand__toggle-btn" onClick={this.toggleContent}>
+            {isExpanded ? (
+              <i className="fas fa-chevron-up" />
+            ) : (
+              <i className="fas fa-chevron-down" />
+            )}
+          </button>
+        </div>
+        {this.state.isExpanded && (
+          <div className="expand__content">{children}</div>
+        )}
       </div>
-      <div className="expand__content">{children}</div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 Expand.propTypes = {
-  isShow: PropTypes.bool.isRequired,
-  children: PropTypes.node.isRequired,
-  title: PropTypes.string.isRequired,
-  onClose: PropTypes.func.isRequired,
+  title: PropTypes.string,
+};
+
+Expand.defaultProps = {
+  title: '',
 };
 
 export default Expand;
